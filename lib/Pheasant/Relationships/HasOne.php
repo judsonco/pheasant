d6b9086941fe28ec77221b8266838381711d935a
Jud <Jud.Stephenson@gmail.com>
2014-12-19 14:59:00 -0500

    Remove undefined method call

--- lib/Pheasant/Relationships/HasOne.php
+++ lib/Pheasant/Relationships/HasOne.php
@@ -34,7 +34,7 @@ class HasOne extends Relationship
         if ($cache) {
             $schema = \Pheasant::instance()->schema($this->class);
             if ($cached = $cache->get($schema->hash($object, array($this->local)))) {
-                return $schema->hydrate($cached)->useCache($cache);
+                return $schema->hydrate($cached);
             }
         }
 

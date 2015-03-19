dc2ea2ed70bb0ea65903e67cfd6c0943b2a6a06a
dad4matic <michael@judson.biz>
2015-03-19 13:39:23 -0500

    removed duplicated logic

--- lib/Pheasant/Schema.php
+++ lib/Pheasant/Schema.php
@@ -69,11 +69,8 @@ class Schema
     {
         $keyValues = array_map(
             function ($k) use ($object) {
-                if(is_array($k)){
-                    return sprintf('%s=%s', $k[1], $object->$k[0]);
-                } else {
-                    return sprintf('%s=%s', $k, $object->$k);
-                }
+                $a = is_array($k);
+                return sprintf('%s=%s',$a?$k[1]:$k, $object->{$a?$k[0]:$k});
             },
             $keys
         );

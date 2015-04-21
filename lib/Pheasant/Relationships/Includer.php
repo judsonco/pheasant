33b985214c657640fe9a23302969f545ee5cf098
Jud <Jud.Stephenson@gmail.com>
2015-04-21 19:32:12 -0400

    Allow HasMany Relationships to be `include`ed

--- lib/Pheasant/Relationships/Includer.php
+++ lib/Pheasant/Relationships/Includer.php
@@ -4,6 +4,7 @@ namespace Pheasant\Relationships;
 
 use \Pheasant\Query\Criteria;
 use \Pheasant\Cache\ArrayCache;
+use \Pheasant\Identity;
 
 /**
  * Finds all possible objects in a relationship that might exist in a query
@@ -26,32 +27,68 @@ class Includer
         $this->_nested = $nested;
     }
 
-    public function loadCache()
+    public function loadCache($object, $key, $alias)
     {
         $this->_cache = new ArrayCache();
-        $rel = $this->_rel;
-        $columnString = array_map(function($l)use($rel){ return (!empty($rel->alias)?$rel->alias.'.':'').$l; }, $this->_rel->local);
-        $ids = iterator_to_array(
-            $this->_query->select(implode(',', $columnString))->groupBy(implode(',', $columnString))->execute()->column()
-        );
+        $rel = $this->_rel->finalForObject($object);
 
-        $queryString = array_map(function($f)use($rel){ return $f.'=?'; }, $this->_rel->foreign);
-        $relatedObjects = \Pheasant::instance()
-            ->finderFor($this->_rel->class)
-            ->find($this->_rel->class, new Criteria(
-                implode(' AND ', $queryString), array($ids))
+
+        $aliasedQueryString = implode(',',
+            array_map(
+                function($local) use($alias) {
+                    return ($alias
+                        ? "{$alias}."
+                        :'').$local;
+                },
+                $rel->local
             )
-            ->includes($this->_nested);
+        );
+        $results = $this->_query
+            ->select($aliasedQueryString)
+            ->execute();
 
-        foreach ($relatedObjects as $obj) {
-            $this->_cache->add($obj);
+        $ids = array();
+        foreach ($results->toArray() as $result) {
+            foreach ($rel->local as $local) $ids []= $result[$local];
+        }
+
+        $relatedObjects = $this->_rel->collectionFor($object, $key, $ids)
+                                     ->includes($this->_nested);
+
+        if (!($this->_rel instanceof HasMany)) {
+            foreach ($relatedObjects as $obj) {
+                $this->_cache->add($obj);
+            }
+        } else {
+            $relatedObjects->restrictBy(
+                array_map(
+                    function ($local, $foreign) {
+                        return array($local, $foreign);
+                    },
+                    $rel->local,
+                    $rel->foreign
+                )
+            );
+
+            foreach ($ids as $id) {
+                $identity = Identity::identityStringFromParams(
+                    $this->_rel->class,
+                    is_array($rel->foreign)
+                        ? array_combine($rel->foreign, (is_array($id) ? $id : array($id)))
+                        : array($rel->foreign, current($id))
+                );
+
+                $collection = clone $relatedObjects;
+                $collection->restrictTo($id);
+                $this->_cache->add($collection, $identity);
+            }
         }
     }
 
-    public function get($object, $key)
+    public function get($object, $key, $alias=null)
     {
-        if(!isset($this->_cache)) {
-            $this->loadCache();
+        if (!isset($this->_cache)) {
+            $this->loadCache($object, $key, $alias);
         }
 
         return $this->_rel->get($object, $key, $this->_cache);
--- tests/Pheasant/Tests/IncludesTest.php
+++ tests/Pheasant/Tests/IncludesTest.php
@@ -58,4 +58,78 @@ class IncludesTest extends \Pheasant\Tests\MysqlTestCase
 
         $this->assertEquals(0, $queries, "this should have hit the cache");
     }
+
+    public function testHasManyIncludesHitsCache()
+    {
+        $queries = 0;
+
+        $this->connection()->filterChain()->onQuery(function ($sql) use (&$queries) {
+            ++$queries;
+
+            return $sql;
+        });
+
+        // the first lookup of SecretIdentity should cache all the rest
+        $heros = Hero::all()->includes(array('Powers'))->toArray();
+        $this->assertNotNull($heros[0]->Powers->toArray());
+        $this->assertEquals(2, $heros[0]->Powers->count());
+        $this->assertEquals(2, count($heros[0]->Powers->toArray()));
+
+        // these should be from cache
+        $queries = 0;
+        $this->assertNotNull($heros[1]->Powers->toArray());
+        $this->assertEquals(2, $heros[1]->Powers->count());
+        $this->assertEquals(2, count($heros[1]->Powers->toArray()));
+
+        $this->assertNotNull($heros[2]->Powers->toArray());
+        $this->assertEquals(2, $heros[2]->Powers->count());
+        $this->assertEquals(2, count($heros[2]->Powers->toArray()));
+
+        $this->assertEquals(0, $queries, "this should have hit the cache");
+    }
+
+    public function testHasManyThroughIncludesHitsCache()
+    {
+        $queries = 0;
+
+        $this->connection()->filterChain()->onQuery(function ($sql) use (&$queries) {
+            ++$queries;
+
+            return $sql;
+        });
+
+        // the first lookup of SecretIdentity should cache all the rest
+        $secret = SecretIdentity::all()->includes(array('Powers'))->toArray();
+        $this->assertNotNull($secret[0]->Powers->toArray());
+        $this->assertEquals(2, $secret[0]->Powers->count());
+        $this->assertEquals(2, count($secret[0]->Powers->toArray()));
+
+        // these should be from cache
+        $queries = 0;
+
+        // Make sure we got the correct info
+        $this->assertEquals('Super-human Strength', $secret[0]->Powers[0]->description);
+        $this->assertEquals('Spider Senses', $secret[0]->Powers[1]->description);
+
+        // Make sure we're not returning null
+        $this->assertNotNull($secret[1]->Powers->toArray());
+        // Make sure the counts are correct
+        $this->assertEquals(2, $secret[1]->Powers->count());
+        $this->assertEquals(2, count($secret[1]->Powers->toArray()));
+        // Make sure the returned information is correct
+        $this->assertEquals('Super-human Strength', $secret[1]->Powers[0]->description);
+        $this->assertEquals('Invulnerability', $secret[1]->Powers[1]->description);
+
+        // Make sure we're not returning null
+        $this->assertNotNull($secret[2]->Powers->toArray());
+        // Make sure the counts are correct
+        $this->assertEquals(2, $secret[2]->Powers->count());
+        $this->assertEquals(2, count($secret[2]->Powers->toArray()));
+        // Make sure the returned information is correct
+        $this->assertEquals('Richness', $secret[2]->Powers[0]->description);
+        $this->assertEquals('Super-human Intellect', $secret[2]->Powers[1]->description);
+
+        // Make sure all of them touched the cache
+        $this->assertEquals(0, $queries, "this should have hit the cache");
+    }
 }
